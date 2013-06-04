f4752e38b17f6819375bb555fa60a88cca960c0a
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2013-06-04 17:06:33 +0300

    Fixed NAFQA-178: Implement utilization PHP Code Sniffer for checking SQL security issue  – fixed pattern for Update queries

--- plugins/SecurityCheck/CodeSniffer/Standards/SQL/Sniffs/RawSQL/UpdateSniff.php
+++ plugins/SecurityCheck/CodeSniffer/Standards/SQL/Sniffs/RawSQL/UpdateSniff.php
@@ -2,8 +2,8 @@
 class SQL_Sniffs_RawSQL_UpdateSniff extends SQL_Sniffs_RawSQL_SniffAbstract
 {
     protected $_accuratePattern = array(
-        '/UPDATE\s+.+\s+SET\s+/i',
-        '/UPDATE\s+(.+\s+)+SET\s+/i',
+        '/(?<!ON)\s*UPDATE\s+.+\s+SET\s+/i',
+        '/(?<!ON)\s*UPDATE\s+(.+\s+)+SET\s+/i',
     );
-    protected $_approximatePattern = '/UPDATE\s+/i';
+    protected $_approximatePattern = '/(?<!ON)\s*UPDATE\s+/i';
 }
