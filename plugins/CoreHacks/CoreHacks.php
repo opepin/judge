b7356a0c700e7a637f25ca5ff7f113f72766df34
Yurii Pochtovyk <satknightn@gmail.com>
2013-06-06 16:07:58 +0300

    Implemented NAFQA-190. Improvements to CoreHacks plugin. Check extension's code pool.

--- plugins/CoreHacks/CoreHacks.php
+++ plugins/CoreHacks/CoreHacks.php
@@ -26,6 +26,15 @@ class CoreHacks extends Plugin
     public function execute($extensionPath)
     {
         parent::execute($extensionPath);
+        $this->_checkCoreClasses();
+        $this->_checkCoreCodePool();
+    }
+    
+    /**
+     * Looks for core classes overrides
+     */
+    protected function _checkCoreClasses()
+    {
         $addionalParams = array(
             'standard'   => __DIR__ . '/CodeSniffer/Standards/CoreHacks',
             'extensions' => 'php',
@@ -37,5 +46,33 @@ class CoreHacks extends Plugin
             array('CoreHacks.Class.Override')
         );
         $this->_addPhpCsIssues($parsedResult, 'corehack');
+    }
+    
+    /**
+     * Looks for files in "core" code pool
+     */
+    protected function _checkCoreCodePool()
+    {
+        $files = array();
+        $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->_extensionPath));
+
+        while($iterator->valid()) {
+            if (!$iterator->isDot()) {
+                if (strpos($iterator->getSubPath(), 'app/code/core') !== false) {
+                    $files[] = $iterator->getSubPathName();
+                }
+            }
+            $iterator->next();
+        }
+        if (!empty($files)) {
+            IssueHandler::addIssue(new Issue( array( 
+                "extension"   => $this->_extensionPath,
+                "checkname"   => $this->_pluginName,
+                "type"        => 'corehack',
+                "comment"     => 'Core Hacks for "core" code pool',
+                "files"       => $files,
+                "occurrences" => count($files),
+            )));
+        }
     }
 }
