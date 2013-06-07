ea51e8189fee505f56d4ad3daeaa5f0714836395
Yurii Pochtovyk <satknightn@gmail.com>
2013-06-07 11:26:22 +0300

    Implemented NAFQA-186. Use Magento's rulesets for phpcs and phpmd - standards changed for CheckStyle

--- plugins/CheckStyle/CheckStyle.php
+++ plugins/CheckStyle/CheckStyle.php
@@ -24,7 +24,7 @@ class CheckStyle extends Plugin
         parent::execute($extensionPath);
         $params = array(
             'ignore'   => '*/jquery*',
-            'standard' => $this->_settings->standardToUse,
+            'standard' => __DIR__ . '/CodeSniffer/Standards/Magento',
             'report'   => 'checkstyle',
         );
 
