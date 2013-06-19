d137e118aa5edf414d060cb891c6900bb4449726
Yurii Pochtovyk <satknightn@gmail.com>
2013-06-19 17:08:37 +0300

    Implemented NAFQA-228. Unfinished code to code lines

--- plugins/CheckComments/CheckComments.php
+++ plugins/CheckComments/CheckComments.php
@@ -3,122 +3,42 @@ namespace CheckComments;
 
 use Netresearch\Logger;
 use Netresearch\IssueHandler;
-use Netresearch\Issue as Issue;
 use Netresearch\Plugin\Plugin as Plugin;
 
 class CheckComments extends Plugin
 {
-    protected $_ncloc = 0;
-    protected $_cloc = 0;
+    CONST PHP_DEPEND_COMMAND = 'vendor/pdepend/pdepend/src/bin/pdepend';
+
+    CONST PHP_DEPEND_TEMP_FILE_NAME_FORMAT = 'php_depend_comments_%s.xml';
 
     /**
-     * Execution command
-     * @var string
-     */
-    protected $_execCommand = 'vendor/pdepend/pdepend/src/bin/pdepend';
-
-    /**
-     * Execute the CheckComments plugin
+     * Execute the CheckComments plugin. Entry point
      */
     protected  function _execute()
     {
-        $lowerBoundary = $this->_settings->lowerBoundary;
-        $upperBoundary = $this->_settings->upperBoundary;
-        $clocToNclocRatio = $this->_getClocToNclocRatio($this->_extensionPath);
-        
-        $failed = ($clocToNclocRatio <= $lowerBoundary || $clocToNclocRatio >= $upperBoundary);
-        IssueHandler::addIssue(new Issue(
-            array('extension' => $this->_extensionPath ,
-                  'checkname' => $this->_pluginName,
-                  'type'      => 'cloc_to_ncloc',
-                  'comment'   => $clocToNclocRatio,
-                  'failed'    => $failed
-            )
-        ));
-
-
-        $unfinishedCodeToNclocRatio = $this->_getUnfinishedCodeToNclocRatio($this->_extensionPath);
-        $failed = $this->_settings->allowedUnfinishedCodeToNclocRatio < $unfinishedCodeToNclocRatio;
-        IssueHandler::addIssue(new Issue(
-            array('extension' => $this->_extensionPath ,
-                'checkname' => $this->_pluginName,
-                'type'      => 'unfinished_code_to_ncloc',
-                'comment'   => $unfinishedCodeToNclocRatio,
-                'failed'    => $failed
-            )
-        ));
-
+        $this->_checkWithPhpDepend();
     }
-
+    
     /**
-     *
-     * calculates the ratio between 'number logical lines of code' and 'number comment lines of code'
-     *
-     * @param string $extensionPath
-     * @return float
-     * @throws \Exception if the ratio cannot be calculated
+     * Checks the extension with PHP Depend
      */
-    protected function _getClocToNclocRatio($extensionPath)
+    protected function _checkWithPhpDepend()
     {
-        $ncloc = 0;
-        $cloc = 0;
-        $metrics = $this->_getMetrics($extensionPath);
-        $this->_ncloc = $metrics['ncloc'];
-        $this->_cloc = $metrics['cloc'];
-        if ((!is_numeric($ncloc) || !is_numeric($cloc)) && $ncloc <= 0) {
-            throw new \Exception('Number of code lines is not numeric or 0? Please check extension path!');
+        $this->setExecCommand(self::PHP_DEPEND_COMMAND);
+        $tmpFilePath = $this->_config->getTempDirPath()
+            . sprintf(self::PHP_DEPEND_TEMP_FILE_NAME_FORMAT, uniqid());
+        $this->_executePhpCommand(array('summary-xml' => $tmpFilePath));
+        $xml = $this->_simplexml_load(file_get_contents($tmpFilePath));
+        unlink($tmpFilePath);
+        if (!$xml) {
+            return;
         }
-        return $this->_cloc / $this->_ncloc;
-    }
-
-
-    protected function _getUnfinishedCodeToNclocRatio($extensionPath)
-    {
-        $unfinishedCode = 0;
-        $precalculatedResults = Logger::getResults($extensionPath, 'CodeRuin');
-        if (!is_null($precalculatedResults)
-            && array_key_exists('resultValue', $precalculatedResults)) {
-            foreach ($precalculatedResults['resultValue'] as $key => $value) {
-                $unfinishedCode += $value;
-            }
+        if (($ncloc = (int)$xml->attributes()->ncloc) === 0) {
+            $ncloc = 1;
         }
-        return $unfinishedCode / $this->_ncloc;
-    }
-
-
-    /**
-     * getting the metrics which are used for calculation
-     * either the metrics came from a previous check or the metrics were calculated
-     *
-     * @param string $extensionPath the extension path
-     * @return array an array containning the *locs
-     */
-    protected function _getMetrics($extensionPath)
-    {
-        $metrics = array();
-        $precalculatedResults = Logger::getResults($extensionPath, 'SourceCodeComplexity');
-        if (!is_null($precalculatedResults)
-            && array_key_exists('resultValue', $precalculatedResults)
-            && array_key_exists('metrics', $precalculatedResults['resultValue'])
-            && array_key_exists('ncloc', $precalculatedResults['resultValue']['metrics'])
-            && array_key_exists('cloc', $precalculatedResults['resultValue']['metrics'])
-        ) {
-            $metrics = $precalculatedResults['resultValue']['metrics'];
-        }
-        if (0 == count($metrics)) {
-            $tempXml = str_replace('.xml', (string) $this->_config->token . '.xml', $this->_settings->tmpXmlFilename);
-            $options = array(
-                'summary-xml' => $tempXml
-            );
-
-            try {
-                $this->_executePhpCommand($options);
-            } catch (\Zend_Exception $e) {
-                return array();
-            }
-            $metrics = current(simplexml_load_file($tempXml));
-            unlink($tempXml);
-        }
-        return $metrics;
+        $this->_addIssue(array(
+            'type'    => 'cloc_to_ncloc',
+            'comment' => round(((float)$xml->attributes()->cloc)/$ncloc, 2),
+        ));        
     }
 }
